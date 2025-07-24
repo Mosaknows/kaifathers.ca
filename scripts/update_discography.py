b6724056b7e2d96fc257092c351d736066c1aed5
Kai <kai.fathers@gmail.com>
2025-07-24 11:14:19 -0400

    Update discography workflow to include GITHUB_TOKEN for secure pushes; remove obsolete single HTML files for various tracks.

--- scripts/update_discography.py
+++ scripts/update_discography.py
@@ -1,4 +1,5 @@
 import os
+import re
 from jinja2 import Environment, FileSystemLoader
 from dotenv import load_dotenv
 import requests
@@ -9,6 +10,13 @@ load_dotenv()
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_ARTIST_ID = '7aOzfiyPyb1w6s6If52cpg'  # Replace with your actual artist ID
+
+def slugify(value):
+    value = value.lower()
+    value = re.sub(r'[^a-z0-9\s-]', '', value)
+    value = re.sub(r'\s+', '-', value)
+    value = re.sub(r'-+', '-', value)
+    return value.strip('-')
 
 # Placeholder for Bandcamp data fetching (to be implemented)
 def fetch_bandcamp_releases():
@@ -26,21 +34,31 @@ def get_spotify_access_token():
 def fetch_spotify_releases():
     access_token = get_spotify_access_token()
     headers = {'Authorization': f'Bearer {access_token}'}
-    albums_url = f'https://api.spotify.com/v1/artists/{SPOTIFY_ARTIST_ID}/albums?include_groups=album,single&limit=50'
+    albums_url = f'https://api.spotify.com/v1/artists/{SPOTIFY_ARTIST_ID}/albums?include_groups=album,single,ep&limit=50'
     response = requests.get(albums_url, headers=headers)
     response.raise_for_status()
     items = response.json().get('items', [])
     releases = []
     for item in items:
-        release_type = 'album' if item['album_type'] == 'album' else 'single'
+        # Determine type for filename
+        album_type = item.get('album_type', 'album')
+        if album_type == 'single':
+            release_type = 'single'
+        elif album_type == 'album':
+            release_type = 'album'
+        elif album_type == 'ep':
+            release_type = 'ep'
+        else:
+            release_type = album_type
         release = {
             'type': release_type,
             'title': item['name'],
-            'slug': item['id'],
+            'slug': slugify(item['name']),
             'cover_url': item['images'][0]['url'] if item['images'] else '',
             'tracks': [],  # Will fetch below
             'description': '',  # Spotify does not provide description
             'spotify_url': item['external_urls']['spotify'],
+            'id': item['id'],
         }
         # Fetch tracklist
         tracks_url = f"https://api.spotify.com/v1/albums/{item['id']}/tracks"
@@ -57,13 +75,24 @@ def main():
     album_template = env.get_template('album_template.html')
     single_template = env.get_template('single_template.html')
 
+    # Track used slugs and their types
+    slug_type_map = {}
+
     # Generate individual release pages
     for release in releases:
-        if release['type'] == 'album':
-            out_path = f"releases/lp-ep/{release['slug']}.html"
+        slug = release['slug']
+        rtype = release['type']
+        filename = slug
+        # If this slug was already used with a different type, append type
+        if slug in slug_type_map and slug_type_map[slug] != rtype:
+            filename = f"{slug}-{rtype}"
+        # Update the map (so future duplicates get type appended)
+        slug_type_map[slug] = rtype
+        if rtype in ['album', 'ep']:
+            out_path = f"releases/lp-ep/{filename}.html"
             html = album_template.render(release=release)
         else:
-            out_path = f"releases/singles/{release['slug']}.html"
+            out_path = f"releases/singles/{filename}.html"
             html = single_template.render(release=release)
         with open(out_path, 'w', encoding='utf-8') as f:
             f.write(html)
